ebcabf6f0e6aa8c02e237973aeed0b2fb84803ab
Giovanni <gvn.gentile@gmail.com>
2021-11-30 18:36:18 -0300

    initial commit

---
+++ src/com/letscode/hello/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.letscode.hello;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        /* Esse aqui comenta
+           intervalo de linhas */
+
+        /**
+         * Esse aqui é o padrão javadoc
+         */
+
+        // Esse aqui comenta uma linha só
+
+        System.out.println("Hello World");
+    }
+}
---
+++ src/com/letscode/hello/SomaNumerosInteiros.java
@@ -0,0 +1,8 @@
+package com.letscode.hello;
+
+public class SomaNumerosInteiros {
+
+    public int soma(int x, int y) {
+        return x + y;
+    }
+}
